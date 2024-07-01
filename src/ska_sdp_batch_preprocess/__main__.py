55b0f90d12c463154e9e4cb12afd92bb8438d030
Ali Taqi <ali.taqi@eng.ox.ac.uk>
2024-07-01 15:48:03 +0100

    HIP-968: parser added to __main__.py

--- src/ska_sdp_batch_preprocess/__main__.py
+++ src/ska_sdp_batch_preprocess/__main__.py
@@ -1,3 +1,26 @@
+import argparse
+
+def parse_args() -> argparse.Namespace:
+    """
+    """
+    parser = argparse.ArgumentParser(
+        description="Batch preprocessing pipeline",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "--config",
+        type=str,
+        required=True,
+        help="input YAML configuration file"
+    )
+    return parser.parse_args()
+
+def main() -> None:
+    """
+    """
+    args = parse_args()
+
+
 if __name__ == "__main__":
     raise NotImplementedError(
         "pipeline not implemented"
