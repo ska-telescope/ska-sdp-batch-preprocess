6f3491198e86ae20e262ee05969e3977ae2bfdbf
Ali Taqi <ali.taqi@eng.ox.ac.uk>
2024-07-01 17:02:24 +0100

    HIP-968: MeasurementSet class started

---
+++ src/ska_sdp_batch_preprocess/operations/ms.py
@@ -0,0 +1,11 @@
+# see license in parent directory
+
+class MeasurementSet:
+    """"""
+
+    def __init__(self):
+        """
+        """
+        raise NotImplementedError(
+            "class not implemented"
+        )
