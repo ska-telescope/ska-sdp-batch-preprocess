eafb223328dadc1052407c0f52a5fbb837d96f88
Ali Taqi <ali.taqi@eng.ox.ac.uk>
2024-07-01 17:12:55 +0100

    HIP-968: data attribute added to MeasurementSet class

--- src/ska_sdp_batch_preprocess/operations/ms.py
+++ src/ska_sdp_batch_preprocess/operations/ms.py
@@ -2,6 +2,10 @@
 
 from pathlib import Path
 
+import numpy as np
+from casacore.tables import table
+from numpy.typing import NDArray
+
 
 class MeasurementSet:
     """"""
@@ -10,3 +14,15 @@ class MeasurementSet:
         """
         """
         self.input_dir = dir
+
+    @property
+    def data(self) -> NDArray:
+        """
+        """
+        try:
+           vis = table(str(self.input_dir)).getcol("DATA")
+        except:
+            raise FileNotFoundError("expected a 'DATA' column")
+        if len(np.asarray(vis).shape) > 4:
+            raise ValueError("unsupported DATA with more than 4 dimensions")
+        return vis
