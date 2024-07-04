0a948f3ff4d9810993c0720942793f7046149485
Ali Taqi <ali.taqi@eng.ox.ac.uk>
2024-07-04 16:52:18 +0100

    HIP-968: processing set reconceptualised to processing intent

---
+++ src/ska_sdp_batch_preprocess/operations/processing_intent.py
@@ -0,0 +1,13 @@
+# see license in parent directory
+
+from xarray.core.dataset import Dataset
+from xradio.vis import read_processing_set
+
+
+class ProcessingIntent:
+    """"""
+
+    def __init__(self, data: Dataset):
+        """
+        """
+        self.data = data
