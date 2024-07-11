d05fb33aa4fe07501bc3401fae3b2bf952adc607
Ali Taqi <ali.taqi@eng.ox.ac.uk>
2024-07-11 20:13:05 +0100

    HIP-974: logs added to pipeline.py

--- src/ska_sdp_batch_preprocess/operations/pipeline.py
+++ src/ska_sdp_batch_preprocess/operations/pipeline.py
@@ -25,16 +25,26 @@ def run(
       YAML configuration parameters read as Python 
       dictionary.
     """
+    logger.info("Entering pipeline")
     if config is not None:
         for func, args in config.items():
             if func.lower() == "convert_msv2_to_msv4":
+                logger.info(f"Converting {msin.name} to MSv4")
                 to_msv4(msin, args)
+                logger.info("Conversion successful")
             elif func.lower() == "load_msv2":
+                logger.info(f"Loading {msin.name} into memory as MSv2")
                 MSv2 = MeasurementSet.ver_2(msin)
             elif func.lower() == "load_msv4":
+                logger.info(f"Loading {msin.name} into memory as MSv4")
                 MSv4 = MeasurementSet.ver_4(msin)
             elif func == "convert_msv2_to_msv4_then_load":
+                logger.info(f"Converting {msin.name} to MSv4")
                 to_msv4(msin, args)
+                logger.info("Conversion successful")
+                logger.info(
+                    f"Loading {msin.with_suffix('.ms4').name} into memory as MSv4"
+                )
                 MSv4 = MeasurementSet.ver_4(
                     msin.with_suffix(".ms4")
                 )
