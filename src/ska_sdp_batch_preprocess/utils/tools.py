82d92b04037dd1686b0b2fffa9eb7927f67c5018
Ali Taqi <ali.taqi@eng.ox.ac.uk>
2024-07-12 16:06:58 +0100

    HIP-974: tools.py docstrings

--- src/ska_sdp_batch_preprocess/utils/tools.py
+++ src/ska_sdp_batch_preprocess/utils/tools.py
@@ -7,6 +7,12 @@ import sys
 
 def reinstate_default_stdout() -> None:
     """
+    Reverts the command line stdout/stderr to 
+    default (i.e., sys.__stdout__ and 
+    sys.__stderr__). Useful where writing to the
+    command line is temporarily blocked via a 
+    context manager but an exception occurs while 
+    executing.
     """
     sys.stdout = sys.__stdout__
     sys.stderr = sys.__stderr__
@@ -14,6 +20,10 @@ def reinstate_default_stdout() -> None:
 @contextlib.contextmanager
 def write_to_devnull():
     """
+    Context manager to disable sdtout/stderr to
+    the command line (via writing to devnull) while
+    running an piece of code, and subsequently
+    reverting to default behaviour.
     """
     sys.stdout = open(os.devnull, 'w')
     sys.stderr = open(os.devnull, 'w')
