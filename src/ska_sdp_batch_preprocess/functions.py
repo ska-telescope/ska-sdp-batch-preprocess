01f557dcd0acea9d51eed798151e722aef7a5897
gkjagwani <gurjeet.jagwani@eng.ox.ac.uk>
2024-07-29 00:20:31 +0100

    HIP-994: Add distributed rfi masking

--- src/ska_sdp_batch_preprocess/functions.py
+++ src/ska_sdp_batch_preprocess/functions.py
@@ -29,12 +29,24 @@ def wrap_time_averager(vis : xr.Dataset, timestep) -> xr.Dataset:
 
 def wrap_freq_averager(vis: xr.Dataset, freqstep) -> xr.Dataset:
     """
-     Wraps the real time processing time averager to work with chunked visibilties
+    Wraps the real time processing frequency averager to work with chunked visibilties
 
     :param: vis, xarray dataset complying to the visibility datamodel
     :param: freqstep,integer value of the number of frequency channels to average
 
-    :return: Time averaged Xarray dataset complying to the visibility datamodel
+    :return: Freq averaged Xarray dataset complying to the visibility datamodel
     """
 
     averaging.averaging_frequency(vis, freqstep, 0.5)
+
+def wrap_mask(vis: xr.Dataset, masks: NDArray[np.float64]) -> xr.Dataset:
+    """
+    Wraps the real time processing rfi masking to work with chunked visibilties
+
+    :param: vis, xarray dataset complying to the visibility datamodel
+    :param: masks, N*2 numpy array, with n pairs of frequency ranges to be masked
+    
+    :return: Xarray dataset complying to the visibility datamodel with masked frequencies
+    """
+
+    rfi_masks.apply_rfi_masks(vis, masks)
--- src/ska_sdp_batch_preprocess/scheduler.py
+++ src/ska_sdp_batch_preprocess/scheduler.py
@@ -36,20 +36,19 @@ def distribute_dummy_np(vis: NDArray[np.float64]) -> NDArray[np.float64]:
 
 def distribute_xr_time_averaging(vis: xr.Dataset, timestep) -> xr.Dataset:
     """
-    Distributes the input visibilties on time and processes them.
+    Distributes the input visibilties on time and averages them.
     Currently only works on local clusters.
     
     :param: vis, xarray dataset complying to the visibility datamodel
     :param: timestep, integer value of the number of timesamples to be averaged
 
-    Returns:
-    Visibility datamodel as an Xarray Dataset
+    :return: Time averaged Xarray dataset complying to the visibility datamodel
     """
 
     cluster = LocalCluster()
     client = Client(cluster)
 
-    chunked_vis = vis.chunk({'baselines':-1, 'frequency':-1, 'polarisation':-1, 'time':100, 'uvw_index':-1})
+    chunked_vis = vis.chunk({'baselines':-1, 'frequency':-1, 'polarisation':-1, 'time':timestep, 'uvw_index':-1})
 
     processed = chunked_vis.map_blocks(functions.wrap_time_averager, kwargs={'timestep': timestep})
 
@@ -60,23 +59,22 @@ def distribute_xr_time_averaging(vis: xr.Dataset, timestep) -> xr.Dataset:
 
     return output
 
-#TODO: Test scaling of distribution on frequency instead of time
+#TODO: Test scaling of distribution on time instead of frequency
 def distribute_xr_freq_averaging(vis: xr.Dataset, freqstep) -> xr.Dataset:
     """
-    Distributes the input visibilties on time and processes them.
+    Distributes the input visibilties on freq and averages them.
     Currently only works on local clusters.
     
     :param: vis, xarray dataset complying to the visibility datamodel
     :param: freqstep, integer value of the number of frequency channels to be averaged
 
-    Returns:
-    Visibility datamodel as an Xarray Dataset
+    :return: Freq averaged Xarray dataset complying to the visibility datamodel
     """
 
     cluster = LocalCluster()
     client = Client(cluster)
 
-    chunked_vis = vis.chunk({'baselines':-1, 'frequency':-1, 'polarisation':-1, 'time':100, 'uvw_index':-1})
+    chunked_vis = vis.chunk({'baselines':-1, 'frequency':freqstep, 'polarisation':-1, 'time':-1, 'uvw_index':-1})
 
     processed = chunked_vis.map_blocks(functions.wrap_freq_averager, kwargs={'freqstep': freqstep})
 
@@ -87,4 +85,27 @@ def distribute_xr_freq_averaging(vis: xr.Dataset, freqstep) -> xr.Dataset:
 
     return output
 
+def distribute_xr_masking(vis: xr.Dataset, masks: NDArray[np.float64], freqstep) -> xr.Dataset:
+    """
+    Distributes the input visibilities on freq and applies masks to them
+    Currently only works on local clusters.
 
+    :param: vis, xarray dataset complying to the visibility datamodel
+    :param: masks, N*2 numpy array, with n pairs of frequency ranges to be masked
+    :param: freqstep, integer value of the number of frequency channels to be distributed
+
+    :return: Xarray dataset complying to the visibility datamodel with masked frequencies
+    """
+    
+    cluster = LocalCluster()
+    client = Client(cluster)
+
+    chunked_vis = vis.chunk({'baselines':-1, 'frequency':freqstep, 'polarisation':-1, 'time':-1, 'uvw_index':-1})
+
+    processed = chunked_vis.map_blocks(functions.wrap_mask, kwargs={'masks': masks})
+    output = processed.compute()
+
+    client.close()
+    cluster.close()
+
+    return output
