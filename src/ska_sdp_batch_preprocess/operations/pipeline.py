27b4975d870b2c74a8196f70c73c760f6850f19e
Ali Taqi <ali.taqi@eng.ox.ac.uk>
2024-08-12 17:19:33 +0100

    HIP-1004: rebasing on top of recent MR

--- src/ska_sdp_batch_preprocess/operations/pipeline.py
+++ src/ska_sdp_batch_preprocess/operations/pipeline.py
@@ -2,12 +2,13 @@
 
 from logging import Logger
 from pathlib import Path
-from ska_sdp_datamodels.visibility.vis_io_ms import create_visibility_from_ms
-import numpy
 from operations.measurement_set import (
     convert_msv2_to_msv4, MeasurementSet
 )
-from ska_sdp_func_python.preprocessing import apply_rfi_masks, averaging_frequency, averaging_time,ao_flagger,rfi_flagger
+from ska_sdp_func_python.preprocessing import (
+    apply_rfi_masks, averaging_frequency, 
+    averaging_time, ao_flagger, rfi_flagger
+)
 
 
 def run(
@@ -30,23 +31,24 @@ def run(
     logger: logging.Logger
       logger object to handle pipeline logs.
     """
-
-    data = create_visibility_from_ms(str(msin))[0]
-    
+    ###### This needs changing ######
+    #data = create_visibility_from_ms(str(msin))[0]
     if config is not None:
-        for process,val in config['processing_functions'].items():
-            if val is None:
-                data = eval(process)(data)
-            else:
-                for params in config['processing_functions'][process]:
-                    if isinstance(config['processing_functions'][process][params], list):
-                        x = numpy.array(config['processing_functions'][process][params]).astype(numpy.float32)
-                        config['processing_functions'][process][params] = x
-                arguments = config['processing_functions'][process]
-                data = eval(process) (data, **arguments)
-        
-
-
+        ###### This needs changing ######
+        #for process,val in config['processing_functions'].items():
+        #    if val is None:
+        #        data = eval(process)(data)
+        #    else:
+        #        for params in config['processing_functions'][process]:
+        #            
+        #            if isinstance(config['processing_functions'][process][params], list):
+        #                
+        #                x = numpy.array(config['processing_functions'][process][params]).astype(numpy.float32)
+        #                config['processing_functions'][process][params] = x
+        #        
+        #        arguments = config['processing_functions'][process]
+        #        
+        #        data = eval(process) (data, **arguments)
         for func, args in config.items():
             if func.lower() == "convert_msv2_to_msv4":
                 logger.info(f"Converting {msin.name} to MSv4")
@@ -62,17 +64,20 @@ def run(
                 logger.info(f"Loading {msin.name} into memory as MSv4")
                 ms = MeasurementSet.ver_4(msin, args, logger=logger)
                 logger.info("Load successful\n  |")
-            
-            elif func == "convert_msv2_to_msv4_then_load":
-                logger.info(f"Converting {msin.name} to MSv4")
-                to_msv4(msin, args, logger=logger)
-                logger.info("Conversion successful\n  |")
-                logger.info(
-                    f"Loading {msin.with_suffix('.ms4').name} into memory as MSv4"
-                )
-                MSv4 = MeasurementSet.ver_4(
-                    msin.with_suffix(".ms4"), logger=logger
-                )
-                logger.info("Load successful\n  |")
 
-    
+            elif func.lower() == "export_to_msv2":
+                logger.info("Exporting list of processing intents to MSv2")
+                ms.export_to_msv2(msin.with_name(f"{msin.stem}-output.ms"))
+                logger.info(f"{msin.stem}-output.ms generated successfully")
+
+            elif func.lower() == "apply_rfi_masks":
+                pass
+
+            elif func.lower() == "averaging_frequency":
+                pass
+
+            elif func.lower() == "averaging_time":
+                pass
+
+            elif func.lower() == "ao_flagger":
+                pass
