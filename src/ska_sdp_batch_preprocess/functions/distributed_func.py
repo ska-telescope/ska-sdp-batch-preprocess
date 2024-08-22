f43245871cf48446fecac28160b5e2e1b38ccb2e
gkjagwani <gurjeet.jagwani@eng.ox.ac.uk>
2024-08-23 00:36:28 +0100

    HIP-999: Functionality to define chunking for all functions and README.md updates

--- src/ska_sdp_batch_preprocess/functions/distributed_func.py
+++ src/ska_sdp_batch_preprocess/functions/distributed_func.py
@@ -5,102 +5,108 @@ import xarray as xr
 from dask.distributed import Client
 from numpy.typing import NDArray
 
-from ska_sdp_func_python.preprocessing.averaging import averaging_time, averaging_frequency
+from ska_sdp_func_python.preprocessing.averaging import (
+    averaging_time, averaging_frequency )
 from ska_sdp_func_python.preprocessing.rfi_masks import apply_rfi_masks
 from ska_sdp_func_python.preprocessing.flagger import rfi_flagger
 
 
-def distribute_averaging_time(vis: xr.Dataset, timestep, chunksize, client: Client, threshold: float) -> xr.Dataset:
+class Distribute:
     """
-    Distributes the input visibilties on time and averages them.
+    Class to easily chunk and divide on frequency and time axis. 
     
-    :param: vis, xarray dataset complying to the visibility datamodel
-    :param: timestep, integer value of the number of timesamples to be averaged
-
-    :return: Time averaged Xarray dataset complying to the visibility datamodel
+    Defines the chunking of the measurement set dependent on the configuration defined by the user.
+    Then distributes the specific processing functions called using DASK on the defined parameters.
     """
-
-    chunked_vis = vis.chunk({'baselines':-1, 'frequency':-1, 'polarisation':-1, 'time':chunksize, 'spatial':-1})
-
-    return client.submit(averaging_time, chunked_vis, timestep, threshold).result()
-
-
-#TODO: Test scaling of distribution on time instead of frequency
-def distribute_averaging_freq(vis: xr.Dataset, freqstep, chunksize, client: Client, threshold: float) -> xr.Dataset:
-    """
-    Distributes the input visibilties on freq and averages them.
     
-    :param: vis, xarray dataset complying to the visibility datamodel
-    :param: freqstep, integer value of the number of frequency channels to be averaged
+    def __init__(self, vis: xr.Dataset, axis, chunksize, client: Client):
+        """
+        Initiates the Distribute class by checking if the user has defined the 
+        chunking axis to be frequency or time and divides the xarray dataset based 
+        on the axis and the chunksize 
+        """
 
-    :return: Freq averaged Xarray dataset complying to the visibility datamodel
-    """
+        if (axis == "frequency"):
+            chunked_axis = {'baselines':-1, 'frequency':chunksize, 'polarisation':-1, 'time':-1, 'spatial':-1}
+        elif (axis == "time"):
+            chunked_axis = {'baselines':-1, 'frequency':-1, 'polarisation':-1, 'time':chunksize, 'spatial':-1}
+        
+        self.vis = vis.chunk(chunked_axis)
+        self.client = client
 
-    chunked_vis = vis.chunk({'baselines':-1, 'frequency':chunksize, 'polarisation':-1, 'time':-1, 'spatial':-1})
+    def avg_time(self, timestep, threshold: float) -> xr.Dataset:
+        """
+        Distributes the input visibilties on time and averages them.
+        
+        :param: timestep, integer value of the number of timesamples to be averaged
+        :param: threshold,
 
-    return client.submit(averaging_frequency, chunked_vis, freqstep, threshold).result()
-
-
-def distribute_rfi_masking(vis: xr.Dataset, masks: NDArray[np.float64], chunksize, client:Client) -> xr.Dataset:
-    """
-    Distributes the input visibilities on freq and applies masks to them
-
-    :param: vis, xarray dataset complying to the visibility datamodel
-    :param: masks, N*2 numpy array, with n pairs of frequency ranges to be masked
-    :param: freqstep, integer value of the number of frequency channels to be distributed
-
-    :return: Xarray dataset complying to the visibility datamodel with masked frequencies
-    """
-
-    chunked_vis = vis.chunk({'baselines':-1, 'frequency':chunksize, 'polarisation':-1, 'time':-1, 'spatial':-1})
-    
-    return client.submit(apply_rfi_masks, chunked_vis, masks).result()
-
-
-def distribute_rfi_flagger(vis: xr.Dataset,
-                           chunksize,
-                           client: Client,
-                           *,
-                           alpha,
-                           threshold_magnitude,
-                           threshold_variation,
-                           threshold_broadband,
-                           sampling,
-                           window,
-                           window_median_history) -> xr.Dataset:
-    """
-    Distributes the input visibilities on time and apllies FluctuFlagger RFI flagger
-    :param vis: xarray dataset complying to the visibility datamodel
-    :param alpha: historical memory coefficient
-    :param threshold_magnitude: threshold for the magnitude
-    :param threshold_variation: threshold for the variations
-    :param threshold_broadband: threshold for the broadband RFI
-    :param sampling: sampling step
-    :param wibdow: window for side channels
-    :param window_median_history: window for broadband
-
-    :return: Xarray dataset complying to the visibility datamodel with flags
-    """
-
-    chunked_vis = vis.chunk({'baselines':-1, 'frequency':-1, 'polarisation':-1, 'time':chunksize, 'spatial':-1})
-
-    return client.submit(rfi_flagger, chunked_vis, alpha, threshold_magnitude, threshold_variation, threshold_broadband, sampling, window, window_median_history).result()
+        :return: Time averaged Xarray dataset complying to the visibility datamodel
+        """
+        self.vis = self.client.submit(averaging_time, self.vis, timestep, threshold).result()
+        return self.vis
 
 
-def distribute_ao_flagger(vis:xr.Dataset, chunksize, client:Client, path=None):
-    """
-    Distributes the input visibilities on time and apllies AOFlagger RFI flagger
-    :param vis: xarray dataset complying to the visibility datamodel
-    :param path: location of the Lua strategy file
-    
-    :return: Xarray dataset complying to the visibility datamodel with flags 
-    """
+    def avg_freq(self, freqstep, threshold: float) -> xr.Dataset:
+        """
+        Distributes the input visibilties on freq and averages them.
+        
+        :param: freqstep, integer value of the number of frequency channels to be averaged
+        :param: threshold, 
 
-    #TODO: Fix aoflagger dependency issue
-    #chunked_vis = vis.chunk({'baselines':-1, 'frequency':-1, 'polarisation':-1, 'time':chunksize, 'spatial':-1})
+        :return: Freq averaged Xarray dataset complying to the visibility datamodel
+        """
 
-    #processed = client.submit(ao_flagger, chunked_vis, path)
+        self.vis = self.client.submit(averaging_frequency, self.vis, freqstep, threshold).result()
+        return self.vis
 
-    #return processed.result()
-    pass
+    def rfi_masking(self, masks: NDArray[np.float64]) -> xr.Dataset:
+        """
+        Distributes the input visibilities on freq and applies masks to them
+
+        :param: masks, N*2 numpy array, with n pairs of frequency ranges to be masked
+
+        :return: Xarray dataset complying to the visibility datamodel with masked frequencies
+        """
+
+        self.vis = self.client.submit(apply_rfi_masks, self.vis, masks).result()
+        return self.vis
+
+    def flagger(self,
+                *,
+                alpha = 0.5,
+                threshold_magnitude = 3.5,
+                threshold_variation = 3.5,
+                threshold_broadband = 3.5,
+                sampling = 8,
+                window = 0,
+                window_median_history = 10) -> xr.Dataset:
+        """
+        Distributes the input visibilities on time and apllies FluctuFlagger RFI flagger
+        :param vis: xarray dataset complying to the visibility datamodel
+        :param alpha: historical memory coefficient
+        :param threshold_magnitude: threshold for the magnitude
+        :param threshold_variation: threshold for the variations
+        :param threshold_broadband: threshold for the broadband RFI
+        :param sampling: sampling step
+        :param wibdow: window for side channels
+        :param window_median_history: window for broadband
+
+        :return: Xarray dataset complying to the visibility datamodel with flags
+        """
+
+        self.vis = self.client.submit(rfi_flagger, self.vis, alpha, threshold_magnitude, threshold_variation, threshold_broadband, sampling, window, window_median_history).result()
+        return self.vis
+
+    def ao_rfi_flagger(self, path=None):
+        """
+        Distributes the input visibilities on time and apllies AOFlagger RFI flagger
+        :param vis: xarray dataset complying to the visibility datamodel
+        :param path: location of the Lua strategy file
+        
+        :return: Xarray dataset complying to the visibility datamodel with flags 
+        """
+
+        #TODO: Fix aoflagger dependency issue
+        pass
 
--- src/ska_sdp_batch_preprocess/operations/pipeline.py
+++ src/ska_sdp_batch_preprocess/operations/pipeline.py
@@ -8,14 +8,10 @@ import numpy as np
 from dask.distributed import Client
 
 from ska_sdp_batch_preprocess.functions.distributed_func import (
-    distribute_averaging_time, distribute_rfi_flagger, 
-    distribute_rfi_masking, distribute_averaging_freq,
+    Distribute
 )
 from ska_sdp_batch_preprocess.operations.measurement_set import (
     convert_msv2_to_msv4, MeasurementSet
-)
-from ska_sdp_batch_preprocess.operations.processing_intent import (
-    ProcessingIntent
 )
 from ska_sdp_batch_preprocess.utils import log_handler, tools
 
@@ -41,8 +37,12 @@ def run(
       logger object to handle pipeline logs.
     """
     if config is not None:
-        
-        if "processing_chain" in config:
+        if "processing_chain" in config:            
+            #Define chunking parameters        
+            axis = config.setdefault("axis", "frequency")
+            chunksize = config.setdefault("chunksize", 10)
+            logger.info(f"DASK distribution - Axis={axis} and Chunksize={chunksize} \n")
+            
             # load MS
             if "load_ms" in config["processing_chain"]:
                 logger.info(f"Loading {msin.name} into memory")
@@ -62,10 +62,14 @@ def run(
                         logger.critical(f"Could not load {msin.name} as either Msv2 or MSv4\n  |")
                         log_handler.exit_pipeline(logger)
 
-                # Run processing functions
-                logger.info("Running requested processing functions ...")
-                processing_functions(ms.dataframe, config["processing_chain"], client, logger=logger)
-                logger.info(f"Successfully ran all processing functions\n  |")
+                #Initialize distributor
+                for data in ms.dataframe:
+                    logger.info("Initializing distribution strategy")
+                    distributor = Distribute(data.data_as_ska_vis, axis, chunksize, client)
+                    # Run processing functions
+                    logger.info("Running requested processing functions ...\n")
+                    processing_functions(distributor, config["processing_chain"],logger=logger)
+                    logger.info(f"Successfully ran all processing functions\n  |")
 
             # export to MSv2
             if "export_to_msv2" in config["processing_chain"]:
@@ -82,56 +86,52 @@ def run(
             logger.info("Conversion successful\n  |")
 
 def processing_functions(
-     ms: list[ProcessingIntent], config: dict, client: Client, *, logger: Logger
+     distributor: Distribute, config: dict,*, logger: Logger
 ) -> None:
         """
         Chain of distributed processing functions that can be configured from the YAML config file.
 
-        param: ms - Measurement Set data represented by the Measurement Set class 
+        param: distributor: Initialised Distribute class that can chunk the visibilites and distribute
+                        them over a DASK cluster
         param: config - Dictionary of configuration parameters read from a YAML file
-        param: client - Dask Client provided by a scheduler or a local cluster
         param: logger - logger class to store and print logs
         """
-        
+
         for func, args in config.items():
+            if args is None:
+                args = {}                
             if func == "apply_rfi_masks":
                 logger.info("Applying rfi masks ...")
-                masks = np.array(args.get('rfi_frequency_masks'), dtype=np.float64)
-                f_chunk = args.get('f_chunk', 4)
-                for data in ms:
-                    data = distribute_rfi_masking(data.data_as_ska_vis, masks, f_chunk, client)
+                masks = np.array(args.setdefault('rfi_frequency_masks', [1.3440e08,1.3444e08]), dtype=np.float64)
+                distributor.rfi_masking(masks)
                 logger.info("Apply rfi masks successful\n  |")
 
             elif func == "averaging_frequency":
                 logger.info("Averaging in frequency ...")
-                freqstep = args.get('freqstep', 4)
-                f_threshold = args.get('flag_threshold', 0.5)
-                f_chunk = args.get('f_chunk', 10)
-                for data in ms:
-                    data = distribute_averaging_freq(data.data_as_ska_vis, freqstep, f_chunk, client, f_threshold)
+                freqstep = args.setdefault('freqstep', 4)
+                f_threshold = args.setdefault('flag_threshold', 0.5)
+                distributor.avg_freq(freqstep, f_threshold) 
                 logger.info("Frequency averaging successful\n  |")
 
             elif func == "averaging_time":
                 logger.info("Averaging in time ...")
-                timestep = args.get('timestep', 4)
-                t_threshold = args.get('flag_threshold', 0.5)
-                t_chunk = args.get('t_chunk', 10)
-                for data in ms:
-                    data = distribute_averaging_time(data.data_as_ska_vis, timestep, t_chunk, client, t_threshold)
+                timestep = args.setdefault('timestep', 4)
+                t_threshold = args.setdefault('flag_threshold', 0.5)
+                distributor.avg_time(timestep, t_threshold)
                 logger.info("Time averaging successful\n  |")
 
             elif func == "rfi_flagger":
                 logger.info("Flagging ...")
-                t_chunk=args.get('t_chunk', 4)
-                alpha=args.get('alpha', 0.5)
-                threshold_magnitude=args.get('magnitude', 3.5)
-                threshold_variation=args.get('variation', 3.5)
-                threshold_broadband=args.get('broadband', 3.5)
-                sampling=args.get('sampling', 8)
-                window=args.get('window', 0)
-                window_median_history= args.get('median_history', 10)
-                for data in ms:
-                    distribute_rfi_flagger(data.data_as_ska_vis, t_chunk, client, alpha=alpha, threshold_magnitude=threshold_magnitude, 
-                                           threshold_variation=threshold_variation, threshold_broadband=threshold_broadband, 
-                                           sampling=sampling, window=window, window_median_history=window_median_history)
+                alpha=args.setdefault('alpha', 0.5)
+                threshold_magnitude=args.setdefault('magnitude', 3.5)
+                threshold_variation=args.setdefault('variation', 3.5)
+                threshold_broadband=args.setdefault('broadband', 3.5)
+                sampling=args.setdefault('sampling', 8)
+                window=args.setdefault('window', 0)
+                window_median_history= args.setdefault('median_history', 10)
+                distributor.flagger(alpha=alpha, threshold_magnitude=threshold_magnitude,
+                                    threshold_variation=threshold_variation,
+                                    threshold_broadband=threshold_broadband,
+                                    sampling=sampling, window=window, 
+                                    window_median_history=window_median_history)
                 logger.info("RFI Flagging successful \n |")
