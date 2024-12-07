a9a4a155e2d878e7776bc2b6a5a9f621a62411be
Vincent Morello <vmorello@gmail.com>
2024-12-07 09:06:44 +0000

    Remove dummy function

--- src/ska_sdp_batch_preprocess/__init__.py
+++ src/ska_sdp_batch_preprocess/__init__.py
@@ -1,8 +1,1 @@
 from ._version import __version__  # noqa: F401
-
-
-def dummy_function() -> int:
-    """
-    Just a dummy function to get development started.
-    """
-    return 0
--- tests/test_dummy_function.py
+++
@@ -1,8 +0,0 @@
-from ska_sdp_batch_preprocess import dummy_function
-
-
-def test_dummy_function():
-    """
-    Self-explanatory.
-    """
-    assert dummy_function() == 0
