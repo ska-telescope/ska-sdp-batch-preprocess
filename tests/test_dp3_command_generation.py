7172f8ed46d961f7dd97bb728a8c66b870d9adf2
Vincent Morello <vmorello@gmail.com>
2024-12-11 15:52:11 +0000

    HIP-1121: First version of config parsing and conversion to DP3 command line

---
+++ tests/test_dp3_command_generation.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+
+import pytest
+
+from ska_sdp_batch_preprocess.config import DP3Config, PipelineConfig
+
+
+@pytest.fixture(name="pipeline_config")
+def fixture_pipeline_config() -> PipelineConfig:
+    """
+    Config for the pipeline. We load it from the example config file provided
+    in the repository.
+    """
+    path = Path(__file__).parent / ".." / "config" / "config.yaml"
+    return PipelineConfig.from_yaml(path)
+
+
+def test_generated_dp3_command_is_correct(pipeline_config: PipelineConfig):
+    """
+    Generate a DP3 command based on the pipeline configuration and check that
+    it is as expected.
+    """
+    msin = Path("/path/to/input.ms")
+    msout = Path("/path/to/output.ms")
+
+    command = DP3Config.create(pipeline_config, msin, msout).to_command_line()
+    expected_command = [
+        "DP3",
+        "steps=[aoflagger,averager]",
+        "msin.name=/path/to/input.ms",
+        "msout.name=/path/to/output.ms",
+        "averager.timeresolution=16.0",
+        "averager.freqresolution=120000.0",
+    ]
+    assert command == expected_command
