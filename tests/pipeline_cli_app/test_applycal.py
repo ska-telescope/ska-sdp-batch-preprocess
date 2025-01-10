b64eccbff47b4a96e417dfad2da777d214c9537e
Vincent Morello <vmorello@gmail.com>
2025-01-10 16:26:34 +0000

    Generate simple scalaramplitude h5parms; test code ready

--- tests/pipeline_cli_app/test_applycal.py
+++ tests/pipeline_cli_app/test_applycal.py
@@ -1,7 +1,9 @@
 import os
 from pathlib import Path
+from typing import Sequence
 
 import casacore.tables
+import h5py
 import numpy as np
 import pytest
 import yaml
@@ -35,6 +37,48 @@ def load_visibilities_from_msv2(
     return vis.reshape(len(unique_timestamps), -1, nchan, npol)
 
 
+def create_scalaramplitude_h5parm(
+    path: Path, *, antenna_names: Sequence[str], amplitude: float
+):
+    """
+    Create an h5parm file containing scalar amplitude gains with a constant
+    value across all axes. We generate a gain table with only the 'ant' axis
+    (antenna names); DP3 then broadcasts the values across all timestamps and
+    frequency channels.
+    """
+
+    def _ndarray_of_null_terminated_bytes(strings: Sequence[str]) -> NDArray:
+        return np.asarray([s.encode() + b"\0" for s in strings])
+
+    num_ants = len(antenna_names)
+
+    with h5py.File(path, mode="w") as file:
+        # The name of the solset is arbitrary
+        group = file.create_group("sol000/amplitude000")
+        # Cast to np.bytes_ because all text attributes must be written as
+        # fixed-length string, otheriwse DP3 complains.
+        group.attrs["TITLE"] = np.bytes_("amplitude")
+
+        # NOTE: We need to null-terminate the antenna names, otherwise DP3
+        # pretends it cannot find them in the H5Parm. I think this is because
+        # it must be loading the antenna names from the MS first, as
+        # null-terminated strings.
+        group.create_dataset(
+            "ant", data=_ndarray_of_null_terminated_bytes(antenna_names)
+        )
+
+        axes = np.bytes_("ant")
+        data_shape = (num_ants,)
+        val_dataset = group.create_dataset(
+            "val", data=np.full(data_shape, amplitude)
+        )
+        val_dataset.attrs["AXES"] = axes
+        weight_dataset = group.create_dataset(
+            "weight", data=np.full(data_shape, 1.0)
+        )
+        weight_dataset.attrs["AXES"] = axes
+
+
 def make_yaml_config_with_applycal_steps(h5parm_paths: list[Path]) -> str:
     """
     Self-explanatory.
@@ -54,8 +98,23 @@ def test_two_applycal_steps_with_gains_that_multiply_into_identity(
     identical.
     """
     tempdir = tmp_path_factory.mktemp("applycal_test")
+
+    antenna_names = getcol(input_ms, "ANTENNA", "NAME")
+    table_twos_path = tempdir / "table_twos.h5parm"
+    create_scalaramplitude_h5parm(
+        table_twos_path, antenna_names=antenna_names, amplitude=2.0
+    )
+    table_halves_path = tempdir / "table_halves.h5parm"
+    create_scalaramplitude_h5parm(
+        table_halves_path, antenna_names=antenna_names, amplitude=0.5
+    )
+
     config_path = tempdir / "config.yml"
-    config_path.write_text(make_yaml_config_with_applycal_steps([]))
+    config_path.write_text(
+        make_yaml_config_with_applycal_steps(
+            [table_twos_path, table_halves_path]
+        )
+    )
 
     output_dir = tempdir / "output_dir"
     output_dir.mkdir()
