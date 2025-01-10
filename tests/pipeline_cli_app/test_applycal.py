9f3db739cf6cbc2b63497d0dd5e8a87af2f2d0ba
Vincent Morello <vmorello@gmail.com>
2025-01-10 15:38:08 +0000

    Check that visibilities are equal

--- tests/pipeline_cli_app/test_applycal.py
+++ tests/pipeline_cli_app/test_applycal.py
@@ -1,8 +1,37 @@
+import os
 from pathlib import Path
 
+import casacore.tables
+import numpy as np
 import pytest
+from numpy.typing import NDArray
 
 from ska_sdp_batch_preprocess.apps.pipeline import run_program
+
+
+def getcol(path: str | os.PathLike, table_name: str, col_name: str):
+    """
+    Returns an entire column for a MeasurementSet.
+    """
+    if table_name in {"", "MAIN"}:
+        spec = str(path)
+    else:
+        spec = f"{str(path)}::{table_name}"
+    with casacore.tables.table(spec, ack=False) as tbl:
+        return tbl.getcol(col_name)
+
+
+def load_visibilities_from_msv2(
+    path: str | os.PathLike, data_column: str = "DATA"
+) -> NDArray:
+    """
+    Loads visibilites from MSv2, assuming it contains rectangular data.
+    Returns complex data with shape (time, baseline, freq, pol).
+    """
+    vis: NDArray = getcol(path, "MAIN", data_column)
+    unique_timestamps = set(getcol(path, "MAIN", "TIME"))
+    __, nchan, npol = vis.shape
+    return vis.reshape(len(unique_timestamps), -1, nchan, npol)
 
 
 def test_two_applycal_steps_with_gains_that_multiply_into_identity(
@@ -10,7 +39,7 @@ def test_two_applycal_steps_with_gains_that_multiply_into_identity(
 ):
     """
     Run DP3 with two applycal steps, applying two gain tables whose product is
-    the identity matrix. We check that the input and output visibilities are
+    the identity matrix. Check that the input and output visibilities are
     identical.
     """
     tempdir = tmp_path_factory.mktemp("applycal_test")
@@ -28,3 +57,7 @@ def test_two_applycal_steps_with_gains_that_multiply_into_identity(
         str(input_ms),
     ]
     run_program(cli_args)
+
+    vis_in = load_visibilities_from_msv2(input_ms)
+    vis_out = load_visibilities_from_msv2(output_dir / input_ms.name)
+    assert np.allclose(vis_in, vis_out)
