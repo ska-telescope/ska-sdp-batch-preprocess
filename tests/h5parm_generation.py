efa6fd4f19860bfe6bd5b28e66627e454d8a1efa
Vincent Morello <vmorello@gmail.com>
2025-01-22 10:48:11 +0000

    Working applycal test with on-the-fly generated scalarphase table Removed H5Parm files, we will now generate them

---
+++ tests/h5parm_generation.py
@@ -0,0 +1,44 @@
+from pathlib import Path
+from typing import Iterable
+
+import h5py
+import numpy as np
+from numpy.typing import NDArray
+
+
+def create_scalarphase_identity_h5parm(
+    path: Path, *, antenna_names: Iterable[str]
+):
+    """
+    Create an H5Parm file containing a 'scalarphase' table where all the phases
+    are uniformly zero.
+    """
+    with h5py.File(path, mode="w") as file:
+        solset = file.create_group("sol000")
+        create_scalarphase_identity_soltab(solset, antenna_names=antenna_names)
+
+
+def create_scalarphase_identity_soltab(
+    solset: h5py.Group, *, antenna_names: Iterable[str]
+):
+    """
+    Self-explanatory.
+    """
+    soltab = solset.create_group("phase000")
+    soltab.attrs["TITLE"] = np.bytes_("phase")
+
+    num_antennas = len(antenna_names)
+
+    soltab.create_dataset(
+        "ant", data=_ndarray_of_null_terminated_bytes(antenna_names)
+    )
+
+    val = soltab.create_dataset("val", data=np.zeros(shape=num_antennas))
+    val.attrs["AXES"] = np.bytes_("ant")
+
+    weight = soltab.create_dataset("weight", data=np.ones(shape=num_antennas))
+    weight.attrs["AXES"] = np.bytes_("ant")
+
+
+def _ndarray_of_null_terminated_bytes(strings: Iterable[str]) -> NDArray:
+    return np.asarray([s.encode() + b"\0" for s in strings])
