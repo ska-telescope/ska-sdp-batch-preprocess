5672be73801dba01ded34f1bfa59313003db1f98
Vincent Morello <vmorello@gmail.com>
2025-01-22 16:22:42 +0000

    Move MS reading code to separate file We want to use that in two places

--- tests/test_applycal.py
+++ tests/test_applycal.py
@@ -1,11 +1,8 @@
-import os
 from pathlib import Path
 from typing import Any, Iterable
 
-import casacore.tables
 import numpy as np
 import pytest
-from numpy.typing import NDArray
 
 from ska_sdp_batch_preprocess.config import parse_config
 from ska_sdp_batch_preprocess.pipeline import Pipeline
@@ -16,6 +13,7 @@ from .h5parm_generation import (
     create_fulljones_identity_h5parm,
     create_scalarphase_identity_h5parm,
 )
+from .ms_reading import getcol, load_visibilities_from_msv2
 
 
 def make_config(h5parm_filenames: Iterable[str]) -> dict[str, Any]:
@@ -25,31 +23,6 @@ def make_config(h5parm_filenames: Iterable[str]) -> dict[str, Any]:
     """
     steps = [{"applycal": {"parmdb": fname}} for fname in h5parm_filenames]
     return {"steps": steps}
-
-
-def getcol(path: str | os.PathLike, table_name: str, col_name: str):
-    """
-    Returns an entire column for a MeasurementSet.
-    """
-    if table_name in {"", "MAIN"}:
-        spec = str(path)
-    else:
-        spec = f"{str(path)}::{table_name}"
-    with casacore.tables.table(spec, ack=False) as tbl:
-        return tbl.getcol(col_name)
-
-
-def load_visibilities_from_msv2(
-    path: str | os.PathLike, data_column: str = "DATA"
-) -> NDArray:
-    """
-    Loads visibilites from MSv2, assuming it contains rectangular data.
-    Returns complex data with shape (time, baseline, freq, pol).
-    """
-    vis: NDArray = getcol(path, "MAIN", data_column)
-    unique_timestamps = set(getcol(path, "MAIN", "TIME"))
-    __, nchan, npol = vis.shape
-    return vis.reshape(len(unique_timestamps), -1, nchan, npol)
 
 
 @skip_unless_dp3_available
