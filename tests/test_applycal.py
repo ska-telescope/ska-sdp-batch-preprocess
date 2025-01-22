c8788076b4f13fafba592852c8bffb0c2f3d2d3f
Vincent Morello <vmorello@gmail.com>
2025-01-22 10:48:11 +0000

    HIP-1149: Working applycal test with on-the-fly generated scalarphase table Removed H5Parm files, we will now generate them

--- tests/test_applycal.py
+++ tests/test_applycal.py
@@ -1,12 +1,17 @@
+import os
 from pathlib import Path
 from typing import Any, Iterable
 
+import casacore.tables
+import numpy as np
 import pytest
+from numpy.typing import NDArray
 
 from ska_sdp_batch_preprocess.config import parse_config
 from ska_sdp_batch_preprocess.pipeline import Pipeline
 
 from .common import skip_unless_dp3_available
+from .h5parm_generation import create_scalarphase_identity_h5parm
 
 
 def make_config(h5parm_filenames: Iterable[str]) -> dict[str, Any]:
@@ -18,6 +23,31 @@ def make_config(h5parm_filenames: Iterable[str]) -> dict[str, Any]:
     return {"steps": steps}
 
 
+def getcol(path: str | os.PathLike, table_name: str, col_name: str):
+    """
+    Returns an entire column for a MeasurementSet.
+    """
+    if table_name in {"", "MAIN"}:
+        spec = str(path)
+    else:
+        spec = f"{str(path)}::{table_name}"
+    with casacore.tables.table(spec, ack=False) as tbl:
+        return tbl.getcol(col_name)
+
+
+def load_visibilities_from_msv2(
+    path: str | os.PathLike, data_column: str = "DATA"
+) -> NDArray:
+    """
+    Loads visibilites from MSv2, assuming it contains rectangular data.
+    Returns complex data with shape (time, baseline, freq, pol).
+    """
+    vis: NDArray = getcol(path, "MAIN", data_column)
+    unique_timestamps = set(getcol(path, "MAIN", "TIME"))
+    __, nchan, npol = vis.shape
+    return vis.reshape(len(unique_timestamps), -1, nchan, npol)
+
+
 @skip_unless_dp3_available
 def test_pipeline_with_multiple_applycal_steps_with_different_h5parm_layouts(
     tmp_path_factory: pytest.TempPathFactory, input_ms: Path
@@ -25,8 +55,13 @@ def test_pipeline_with_multiple_applycal_steps_with_different_h5parm_layouts(
     """
     Self-explanatory.
     """
-    config = make_config(["scalarphase.h5", "diagonal.h5", "fulljones.h5"])
-    solutions_dir = Path(__file__).parent / "data"
+    antenna_names = getcol(input_ms, "ANTENNA", "NAME")
+    solutions_dir = tmp_path_factory.mktemp("applycal_solutions_dir")
+    create_scalarphase_identity_h5parm(
+        solutions_dir / "scalarphase.h5", antenna_names=antenna_names
+    )
+    config = make_config(["scalarphase.h5"])
+
     output_dir = tmp_path_factory.mktemp("applycal_outdir")
     output_ms = output_dir / input_ms.name
 
@@ -35,3 +70,8 @@ def test_pipeline_with_multiple_applycal_steps_with_different_h5parm_layouts(
     pipeline.run(input_ms, output_ms)
 
     assert output_ms.is_dir()
+
+    vis_in = load_visibilities_from_msv2(input_ms)
+    vis_out = load_visibilities_from_msv2(output_ms)
+
+    assert np.allclose(vis_in, vis_out)
