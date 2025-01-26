90384fc813e9482bcd8ab98135c8ab6e916d4390
Vincent Morello <vmorello@gmail.com>
2025-01-26 18:59:48 +0000

    HIP-1164: Prevent workers from running more than 1 DP3 task at once, using dask resource

--- src/ska_sdp_batch_preprocess/application.py
+++ src/ska_sdp_batch_preprocess/application.py
@@ -1,12 +1,18 @@
 import logging
 from pathlib import Path
-from typing import Iterable, Optional
+from typing import Any, Iterable, Optional
 
 import dask
 import dask.distributed
 
 from ska_sdp_batch_preprocess.logging_setup import LOGGER
 from ska_sdp_batch_preprocess.pipeline import Pipeline
+
+SUBPROCESS_RESOURCE = "subprocess"
+"""
+Name of the dask resource used to limit the number of DP3 subprocesses
+run simultaneously by dask workers.
+"""
 
 
 class Application:
@@ -46,13 +52,15 @@ class Application:
         scheduler network address.
         """
         client = dask.distributed.Client(dask_scheduler, timeout=5.0)
+        _assert_at_least_one_worker_with_subprocess_resource(client)
         client.forward_logging(LOGGER.name, level=logging.DEBUG)
-        futures = client.compute(
-            [
+
+        with dask.annotate(resources={SUBPROCESS_RESOURCE: 1}):
+            delayed_list = [
                 dask.delayed(self._process_ms_on_dask_worker)(input_ms)
                 for input_ms in map(Path.resolve, input_mses)
             ]
-        )
+        futures = client.compute(delayed_list)
         dask.distributed.wait(futures)
 
     def _process_ms_on_dask_worker(self, input_ms: Path):
@@ -62,4 +70,18 @@ class Application:
             input_ms,
             self._output_dir / input_ms.name,
             numthreads=worker.state.nthreads,
+        )
+
+
+def _assert_at_least_one_worker_with_subprocess_resource(
+    client: dask.distributed.Client,
+):
+    def _usable_worker(worker: dict[str, Any]) -> bool:
+        resources: dict[str, float] = worker["resources"]
+        return resources.get(SUBPROCESS_RESOURCE, 0) >= 1
+
+    workers_dict: dict[str, dict] = client.scheduler_info()["workers"]
+    if not any(map(_usable_worker, workers_dict.values())):
+        raise RuntimeError(
+            f"Found no workers with resource {SUBPROCESS_RESOURCE!r} >= 1"
         )
