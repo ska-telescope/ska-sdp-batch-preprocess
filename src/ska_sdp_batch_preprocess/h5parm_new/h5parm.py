921708a1e8c3f42ac2ec565fb239ef534d729109
Vincent Morello <vmorello@gmail.com>
2025-02-14 19:30:56 +0000

    Add H5Parm top-level validation

--- src/ska_sdp_batch_preprocess/h5parm_new/h5parm.py
+++ src/ska_sdp_batch_preprocess/h5parm_new/h5parm.py
@@ -6,7 +6,7 @@ import h5py
 import numpy as np
 from numpy.typing import ArrayLike
 
-from .assertions import assert_or_invalid_h5parm
+from .assertions import InvalidH5Parm, assert_or_invalid_h5parm
 from .soltab import (
     WEIGHTS_DTYPE,
     Soltab,
@@ -24,6 +24,7 @@ class H5Parm:
     - Only one solset
     - Only one or two soltabs; if there are two soltabs, they must be of type
       "amplitude" and "phase".
+    - The "antenna" and "source" datasets
     """
 
     def __init__(self, soltabs: Iterable[Soltab]):
@@ -34,15 +35,7 @@ class H5Parm:
             soltabs: Iterable containing Soltab instances.
         """
         self.__soltabs = tuple(soltabs)
-        self.__validate()
-
-    def __validate(self):
-        # NOTE: soltabs are assumed to be validated already
-        # Refuse nsoltabs not in (1, 2)
-        # if nsoltabs == 1, it can't be phase or amplitude with 4 pols
-        # if nsoltabs == 2, check they are phase and amplitude, and consistent
-        # with each other
-        pass
+        validate_h5parm(self)
 
     @property
     def soltabs(self) -> tuple[Soltab]:
@@ -56,7 +49,7 @@ class H5Parm:
         Save to given file path.
         """
         with h5py.File(path, "w") as file:
-            # NOTE: group names for solsets and soltabs should not matter,
+            # Group names for solsets and soltabs should not matter,
             # but let's follow the LOFAR convention
             solset = file.create_group("sol000")
             for soltab in self.soltabs:
@@ -119,3 +112,38 @@ class H5Parm:
 
     def __repr__(self) -> str:
         return str(self)
+
+
+def validate_h5parm(parm: H5Parm):
+    """
+    Self-explanatory.
+    """
+
+    def _pols(tab: Soltab) -> tuple[str]:
+        return tuple(tab.axes.get("pol", []))
+
+    if len(parm.soltabs) == 1:
+        (tab,) = parm.soltabs
+        incomplete_full_jones = (
+            tab.soltype in {"amplitude", "phase"} and len(_pols(tab)) == 4
+        )
+        assert_or_invalid_h5parm(
+            not incomplete_full_jones,
+            f"H5Parm contains only the {tab.soltype} part of a "
+            "full Jones solution",
+        )
+
+    elif len(parm.soltabs) == 2:
+        first, second = parm.soltabs
+        assert_or_invalid_h5parm(
+            {first.soltype, second.soltype} == {"amplitude", "phase"},
+            "The solution types of a 2-Soltab H5Parm must be amplitude and "
+            "phase",
+        )
+        assert_or_invalid_h5parm(
+            _pols(first) == _pols(second),
+            "Soltabs in H5Parm must have the same pol axis",
+        )
+
+    else:
+        raise InvalidH5Parm("Number of soltabs must be 1 or 2")
