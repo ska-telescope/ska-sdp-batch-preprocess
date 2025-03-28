451b6db2af8eabaf9add6147f9c7eb3b47ff633d
Vincent Morello <vmorello@gmail.com>
2025-03-28 19:11:22 +0000

    Update DP3 command line test

--- src/ska_sdp_batch_preprocess/pipeline/__init__.py
+++ src/ska_sdp_batch_preprocess/pipeline/__init__.py
@@ -1,7 +1,19 @@
+from .dp3_parametrisation import (
+    format_dp3_parameters,
+    make_dp3_command_line,
+    make_dp3_parameters,
+)
 from .dp3_params import DP3Params
 from .pipeline import Pipeline
 
 # NOTE: this should be removed
 from .step_preparation import PreparedStep
 
-__all__ = ["DP3Params", "Pipeline", "PreparedStep"]
+__all__ = [
+    "DP3Params",
+    "Pipeline",
+    "PreparedStep",
+    "format_dp3_parameters",
+    "make_dp3_command_line",
+    "make_dp3_parameters",
+]
--- tests/test_dp3_command_generation.py
+++ tests/test_dp3_command_generation.py
@@ -3,33 +3,34 @@ from pathlib import Path
 
 import pytest
 
-from ska_sdp_batch_preprocess.pipeline import DP3Params, PreparedStep
+from ska_sdp_batch_preprocess.config import Step
+from ska_sdp_batch_preprocess.pipeline import (
+    Pipeline,
+    format_dp3_parameters,
+    make_dp3_command_line,
+    make_dp3_parameters,
+)
 
 
 @pytest.fixture(name="steps")
-def fixture_steps() -> list[PreparedStep]:
+def fixture_steps() -> list[Step]:
     """
     List of pipeline steps for which we want to generate a DP3 call.
     """
 
     return [
-        PreparedStep(type="preflagger", name="preflagger_01", params={}),
-        PreparedStep(
-            type="aoflagger", name="aoflagger_01", params={"memorymax": 8.0}
-        ),
-        PreparedStep(
+        Step(type="preflagger", params={}),
+        Step(type="aoflagger", params={"memorymax": 8.0}),
+        Step(
             type="averager",
-            name="averager_01",
             params={"timestep": 4, "freqstep": 4},
         ),
-        PreparedStep(
-            type="aoflagger", name="aoflagger_02", params={"memorymax": 16.0}
-        ),
-        PreparedStep(type="msout", name="msout", params={"overwrite": True}),
+        Step(type="aoflagger", params={"memorymax": 16.0}),
+        Step(type="msout", params={"overwrite": True}),
     ]
 
 
-def test_generated_dp3_command_is_correct(steps: list[PreparedStep]):
+def test_generated_dp3_command_is_correct(steps: list[Step]):
     """
     Generate a DP3 command based on the given pipeline steps, check that
     it is as expected.
@@ -37,7 +38,10 @@ def test_generated_dp3_command_is_correct(steps: list[PreparedStep]):
     msin = Path("/path/to/input.ms")
     msout = Path("/path/to/output.ms")
 
-    command = DP3Params.create(steps, msin, msout).to_command_line()
+    pipeline = Pipeline(steps)
+    params = make_dp3_parameters(pipeline.steps, msin, msout)
+    command = make_dp3_command_line(format_dp3_parameters(params))
+
     expected_numthreads = len(os.sched_getaffinity(0))
     expected_command = [
         "DP3",
