5e18e9a8b70faeaf07efc8992a472da0567cd7d4
Vincent Morello <vmorello@gmail.com>
2025-04-10 14:41:47 +0100

    Try to factorise the code that makes the column descriptors

--- src/ska_sdp_batch_preprocess/mswriter/core.py
+++ src/ska_sdp_batch_preprocess/mswriter/core.py
@@ -2,6 +2,9 @@ import os
 from pathlib import Path
 from xarray import DataTree
 import yaml
+import copy
+
+from typing import Any, Type
 
 from casacore.tables import table, maketabdesc, makescacoldesc, makearrcoldesc
 
@@ -51,6 +54,42 @@ def write_msv2_template_matching_xradio_msv4(
     write_antenna_table(msv4, output_path)
 
 
+def make_column_description(
+    table_name: str, column_name: str, shape_dict: dict[str, int]
+) -> dict[str, Any]:
+    """
+    TODO
+    """
+    schema = SCHEMA[table_name][column_name]
+    kind = schema["kind"]
+    vtype: Type = eval(schema["type"])
+
+    def make_shape(shape_spec: list[str | int]) -> tuple[int]:
+        return tuple(x if isinstance(x, int) else shape_dict[x] for x in shape_spec)
+
+    def tiled_data_manager_group_name(colname: str) -> str:
+        return "Tiled" + "".join(map(str.capitalize, colname.split("_")))
+
+    if kind == "scalar":
+        return makescacoldesc(
+            column_name,
+            vtype(),
+            datamanagergroup=column_name,
+            keywords=schema.get("keywords", {}),
+        )
+    elif kind == "array":
+        return makearrcoldesc(
+            column_name,
+            vtype(),
+            shape=make_shape(schema["shape"]),
+            datamanagertype="TiledColumnStMan",
+            datamanagergroup=tiled_data_manager_group_name(column_name),
+            keywords=schema.get("keywords", {}),
+        )
+    else:
+        raise ValueError(f"Invalid column kind {kind!r}")
+
+
 def write_main_table_template(msv4: DataTree, output_path: str | os.PathLike):
     """
     TODO
@@ -58,103 +97,17 @@ def write_main_table_template(msv4: DataTree, output_path: str | os.PathLike):
     msv4: Root node of one MSv4
     output_path: Path to base directory of the output MS
     """
-    scalar_column_definitions = {
-        "ANTENNA1": int,
-        "ANTENNA2": int,
-        "ARRAY_ID": int,
-        "DATA_DESC_ID": int,
-        "EXPOSURE": float,
-        "FEED1": int,
-        "FEED2": int,
-        "FIELD_ID": int,
-        "FLAG_ROW": bool,
-        "INTERVAL": float,
-        "OBSERVATION_ID": int,
-        "PROCESSOR_ID": int,
-        "SCAN_NUMBER": int,
-        "STATE_ID": int,
-        "TIME": float,
-        "TIME_CENTROID": float,
+    shape_dict = {
+        "nchan": msv4["frequency"].size,
+        "npol": 4,
     }
 
-    scalar_column_descriptors = [
-        makescacoldesc(name, valuetype(), datamanagergroup=name)
-        for name, valuetype in scalar_column_definitions.items()
+    column_descriptors = [
+        make_column_description("MAIN", column_name, shape_dict=shape_dict)
+        for column_name in SCHEMA["MAIN"]
     ]
 
-    nchan: int = msv4["frequency"].size
-    npol = 4
-
-    array_column_definitions = {
-        "DATA": (complex, (nchan, npol)),
-        "FLAG": (bool, (nchan, npol)),
-        "FLAG_CATEGORY": (bool, (1, nchan, npol)),  # enforcing one category
-        "SIGMA": (float, (nchan,)),
-        "UVW": (float, (3,)),
-        "WEIGHT": (float, (npol,)),
-        "WEIGHT_SPECTRUM": (float, (nchan, npol)),
-    }
-
-    ### MeerKAT nano
-    # DATA
-    # {'UNIT': 'Jy'}
-
-    # EXPOSURE
-    # {'QuantumUnits': ['s']}
-
-    # FLAG_CATEGORY
-    # {'CATEGORY': []}
-
-    # INTERVAL
-    # {'QuantumUnits': ['s']}
-
-    # TIME
-    # {'QuantumUnits': ['s'], 'MEASINFO': {'type': 'epoch', 'Ref': 'UTC'}}
-
-    # TIME_CENTROID
-    # {'QuantumUnits': ['s'], 'MEASINFO': {'type': 'epoch', 'Ref': 'UTC'}}
-
-    # UVW
-    # {'QuantumUnits': ['m', 'm', 'm'], 'MEASINFO': {'type': 'uvw', 'Ref': 'ITRF'}}
-
-    ### Simulated AA2 Mid datset
-    # DATA
-    # {'UNIT': 'Jy'}
-
-    # EXPOSURE
-    # {'QuantumUnits': ['s']}
-
-    # FLAG_CATEGORY
-    # {'CATEGORY': []}
-
-    # INTERVAL
-    # {'QuantumUnits': ['s']}
-
-    # TIME
-    # {'QuantumUnits': ['s'], 'MEASINFO': {'type': 'epoch', 'Ref': 'UTC'}}
-
-    # TIME_CENTROID
-    # {'QuantumUnits': ['s'], 'MEASINFO': {'type': 'epoch', 'Ref': 'UTC'}}
-
-    # UVW
-    # {'QuantumUnits': ['m', 'm', 'm'], 'MEASINFO': {'type': 'uvw', 'Ref': 'ITRF'}}
-
-    def data_manager_group(colname: str) -> str:
-        return "Tiled" + "".join(map(str.capitalize, colname.split("_")))
-
-    # TODO: Add keywords 'QuantumUnits' and 'MEASINFO' for columns:
-    # DATA, EXPOSURE, FLAG_CATEGORY, INTERVAL, TIME, TIME_CENTROID
-    array_column_descriptors = [
-        makearrcoldesc(
-            name,
-            valuetype(),
-            shape=shape,
-            datamanagertype="TiledColumnStMan",
-            datamanagergroup=data_manager_group(name),
-        )
-        for name, (valuetype, shape) in array_column_definitions.items()
-    ]
-    tdesc = maketabdesc(scalar_column_descriptors + array_column_descriptors)
+    tdesc = maketabdesc(column_descriptors)
     ms_table = table(output_path, tdesc, nrow=0)
     ms_table.close()
 
