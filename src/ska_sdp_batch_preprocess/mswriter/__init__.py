69da3e39272cd9bdf1218055e6c7f16e2a4e7664
Vincent Morello <vmorello@gmail.com>
2025-04-11 09:01:03 +0100

    Test RASCIL's MAIN table writer

--- src/ska_sdp_batch_preprocess/mswriter/__init__.py
+++ src/ska_sdp_batch_preprocess/mswriter/__init__.py
@@ -1,4 +1,5 @@
 from .core import (
     write_msv2_template_matching_xradio_processing_set,
     write_msv2_template_matching_xradio_msv4,
+    rascil_write_msv2_template_matching_xradio_processing_set,
 )
